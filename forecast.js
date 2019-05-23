00ea1dd6c92d76f148c2c4401cfb3a7294093f68
sms7182 <sms7182@gmail.com>
2019-05-23 12:34:03 +0430

    add forecast

---
+++ forecast.js
@@ -0,0 +1,17 @@
+const request=require('request');
+
+const forecast=(latitude,logitude,callback)=>{
+  const url='https://api.darksky.net/forecast/1d2c0ed3bf7c79f5c7d1dd373f7cd995/'+latitude+','+logitude;
+  request({url:url,json:true},(error,response)=>{
+    if(error){
+        callback('Unable to connect to weather service!',undefined);
+    }
+    else if(response.body.error){
+        callback('Unable to find location',undefined);
+    }
+    else {
+        callback(undefined,response.body.daily.data[0].summary+'It is currently')
+    }
+  }); 
+}
+module.exports=forecast;
